cafae0d1bd11b92a036748d852535ea8f673047b
Raghvendra Singh <raghvendrrsingh@gmail.com>
2021-10-18 20:11:51 +0530

    Fixed visa logo bug

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function changeFunc() {
     var selectBox = document.getElementById("selectBox");
     var selectedValue = selectBox.options[selectBox.selectedIndex].value;
     if (selectedValue == "Visa") {
-        document.getElementById("cardlogo").src = "https://lh3.googleusercontent.com/proxy/6Dnb1BBMZMw9-QjhHGzd4brLn_oA_6EL5hEv6z0lc5-EjovS-N4wgR38vmF_tJtNI7Pqd0hpc5SCW9G4qLcZ1mfUVopIG-mgGEkXB6tBe2pj3pX14JWSptqCYyIy0VK0";
+        document.getElementById("cardlogo").src = "./visa.png";
         document.getElementById("cardlogo").style.width = "auto";
         document.getElementById("cardlogo").style.height = "70px";
     }
